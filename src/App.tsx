85d3423d6a4c98b8f181db4cd904d91e54c52970
Sudikshaw7 <sudikshawandre@gmail.com>
2026-06-20 15:50:28 +0530

    fix1

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <WhatsAppPopup />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename="event-sc.vercel.app">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/events" element={<Events />} />
